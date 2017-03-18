fca597fb88fd992125b510f901defc22d043e3a0
jac32 <jac32@st-andrews.ac.uk>
2017-03-18 14:27:26 +0000

    world and entity system

--- game/main.py
+++ game/main.py
@@ -1,16 +1,32 @@
 #!/usr/bin/python
 
-class Ship:
+class Entity():
     def __init__(self):
-       print("ship built") 
+       print("entity built")
+
+    def identify(self):
+        print("I am an entity")
+
+class Ship(Entity):
+    def __init__(self):
+        Entity.__init__(self)
+        print("ship built")
+
+    def identify(self):
+        print("I am an ship")
+
+class World():
+    def __init__(self):
+        print("Created world")
+        self.entities = []
+
+    def register(self, entity):
+        self.entities.append(entity)
 
 def main():
-    print("Hello, World!")
-    print("Making a ship...")
-    ship = Ship()
-    print(ship)
-
+    world = World()
+    world.register(Ship())
+    print(world.entities)
 
 if __name__ == "__main__":
     main()
-
