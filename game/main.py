514345c4a840064f047d65ab13e585c386f345a0
jac32 <jac32@st-andrews.ac.uk>
2017-03-18 14:03:20 +0000

    ship object

--- game/main.py
+++ game/main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python
 
+class Ship:
+    def __init__(self):
+       print("ship built") 
+
 def main():
-    print("Hello")
+    print("Hello, World!")
+    print("Making a ship...")
+    ship = Ship()
+    print(ship)
+
 
 if __name__ == "__main__":
     main()
+
